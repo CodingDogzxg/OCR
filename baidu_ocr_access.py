d189f9e869f680c797b77f3c8ac1443fa84497c9
CodingDog_zxg <49633098+CodingDogzxg@users.noreply.github.com>
2020-03-09 20:33:56 +0800

    Update baidu_ocr_access.py

--- baidu_ocr_access.py
+++ baidu_ocr_access.py
@@ -2,7 +2,9 @@
 import requests
 
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=【官网获取的AK】&client_secret=【官网获取的SK】'
+AK = ''
+SK = ''  # AK SK是百度AI的access API key &　secret key
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(AK, SK)
 response = requests.get(host)
 if response:
     print(response.json())
